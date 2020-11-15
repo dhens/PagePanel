0bb7477dc77661fc8f529a73dbacd8272354a321
Dawson Hensel <dawsonhensel@gmail.com>
2020-11-14 18:48:36 -0800

    server.js: added dotenv API_PORT variable for server listening & app.get route for generic routing. added getPageStatus-Parse.js, which will parse the request responses fed to it by api.js, which uses an axios get request to get data.

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const API_PORT= process.env.API_PORT;
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+})
+
 // START SERVER LISTEN
 app.listen(API_PORT, () => {
     console.log(`API Server listening on ${API_PORT}`);
