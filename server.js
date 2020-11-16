662efa7628f92f4fdce1f47281575af59c994199
Dawson Hensel <dawsonhensel@gmail.com>
2020-11-15 16:50:36 -0800

    server.js: added api.js to linked files, added API_PORT from dotenv, added post route (this currently returns a undefined to the user when they send a url to the post route, this will be fixed in my next push. api.js now had 2 functions, getPageDom (prviously known as getPageStatus), and parsePage, which currenty only loads the request response returned by getPageDom. removed getPageStatus-Parse.js as all future api functions will live in the api.js file

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const api = require('./utils/api');
+const API_PORT = process.env.API_PORT;
 
-const API_PORT= process.env.API_PORT;
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
+// ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome!');
-})
+});
+
+app.post('/page', (req, res) => {
+    const asyncGetPage = async (requestUrl) => {
+        return api.getPageDom(requestUrl);
+    }
+    asyncGetPage(req.body.message)
+        .then(r => console.log(r))
+});
 
 // START SERVER LISTEN
 app.listen(API_PORT, () => {
     console.log(`API Server listening on ${API_PORT}`);
-  });
+});
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,23 @@
-import axios from 'axios';
+"use strict";
 
-export default {
-    getPageStatus: pageUrl => {
-        return axios.get(pageUrl)
+const axios = require('axios');
+module.exports =  {
+    getPageDom: pageUrl => {
+        axios.get(pageUrl, {
+            params: {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+            }
+        })
+        .then(function(success) {
+            return success.data;
+        })
+        .catch(function (error) {
+            return error.message;
+        })
+    },
+
+    parsePage: request => {
+        let pageToParse = getPageStatus(request);
+        return cheerio.load(pageToParse);
     }
 }
