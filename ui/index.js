44e911f6e40ca4f9ef727e27e5f9a2cf1453c3f8
Dawson Hensel <dawsonhensel@gmail.com>
2020-12-08 16:18:52 -0800

    index.js: fixed 'SyntaxError: Unexpected token < in JSON at position 0' by new method of making fetch POST request. results return the DOM code to the console of the electron client!

--- ui/index.js
+++ ui/index.js
@@ -1,29 +1,33 @@
+// DOM ELEMENTS
 const urlInputField = document.getElementById('url-input-field');
 const submitUrlInputBtn = document.getElementById('submit-url-input');
 const submitUrlInputBtnDefaultState = submitUrlInputBtn.className;
 
+// DOM ELEMENT ANIMATIONS
+const submitUrlInputBtnIsLoading = submitUrlInputBtn.className + ' is-loading';
+
 const sanitizeInput = input => {
-    
 }
 
 // Listening Events
 submitUrlInputBtn.addEventListener('click', event => {
     event.preventDefault();
-    submitUrlInputBtn.className = submitUrlInputBtn.className + ' is-loading'
+    submitUrlInputBtn.className = submitUrlInputBtnIsLoading;  // toggle loading icon for Track! btn
 
-    fetch('http://localhost:8004/page', {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const submittedUrl = JSON.stringify({ "message": `${urlInputField.value}` });
+
+    const requestOptions = {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: urlInputField.textContent,
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(`Success: ${JSON.stringify(data)}`)
-        submitUrlInputBtn.className = submitUrlInputBtnDefaultState;
-    })
-    .catch((error) => {
-        console.log(`Error: ${error}`)
-    })
+        headers: myHeaders,
+        body: submittedUrl,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:8004/page", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 });
