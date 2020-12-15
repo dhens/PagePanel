25b1bde3de15c8b4bab96344d399210c69a9766c
Dawson Hensel <dawsonhensel@gmail.com>
2020-12-14 17:53:25 -0800

    changed index.js to formActions.js to be more precise in what the file contains. added boilerplate code to db.js from https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB. will start to structure indexedDB in next push

--- utils/db.js
+++ utils/db.js
@@ -3,3 +3,25 @@
 if (!window.indexedDB) {
     console.log("Your browser doesn't support a stable version of IndexedDB. Saving feature will not be available.");
 }
+
+let request = window.indexedDB.open('SavedPagesDb', 3);
+
+request.onerror = event => {
+// Generic error handler for all errors targeted at this database's
+// requests!
+    console.error("Database error: " + event.target.errorCode);
+};
+
+request.onsuccess = event => {
+    // DO something with request.result
+    db = event.target.result;
+};
+
+// This event is only implemented in recent browsers
+request.onupgradeneeded = function(event) {
+    // Save the IDBDatabase interface
+    var db = event.target.result;
+  
+    // Create an objectStore for this database
+    var objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+  };
