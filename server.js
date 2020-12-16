5b678ae5835e9c90aecb6751150938d6160fd5cf
Dawson Hensel <dawsonhensel@gmail.com>
2020-12-15 18:42:59 -0800

    Added comments to backend URL validation o /page POST route. added actionNotification.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const { body, validationResult } = require('express-validator'); 
+const { body, validationResult } = require('express-validator');
 const API_PORT = process.env.API_PORT;
 
 // MIDDLEWARE
@@ -16,20 +16,15 @@ app.get('/', (req, res) => {
 
 // convert url to DOM data and send back to client
 app.post('/page', [
-    body('message').isURL(),
+    body('message').isURL(),    // Validate that the data submitted is a URL
     body('notifyOnReply').toBoolean()
 ], (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
+    if (!errors.isEmpty()) {    // If there are errors, return a 400 with the errors to the client
         console.log('/page POST validation failed: ' + req.body.message)
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
-    if (req.body.message === '') {
-        console.log('Error: Blank string submitted');
-        res.send('Error: Blank string submitted');
-        return;
-    }
-    axios.get(req.body.message)
+    axios.get(req.body.message) // Grab the DOM data of the user submitted URL
         .then(function (response) {
             res.send(response.data);
         })
